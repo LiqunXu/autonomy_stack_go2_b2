a9f3ebc82ade84b214e24630a31589f2c74500ce
Liqun Xu <liqunxu@LiqundeMacBook-Pro.local>
2025-03-18 22:04:45 -0500

    test transform in transform_everything.py

--- src/utilities/transform_sensors/transform_sensors/transform_everything.py
+++ src/utilities/transform_sensors/transform_sensors/transform_everything.py
@@ -73,7 +73,7 @@ class Repuber(Node):
         self.body2cloud_trans.transform.translation.x = 0.0
         self.body2cloud_trans.transform.translation.y = 0.0
         self.body2cloud_trans.transform.translation.z = 0.0
-        quat = tf_transformations.quaternion_from_euler(0, 0, 0)
+        quat = tf_transformations.quaternion_from_euler(3.14159265358, 0, 0)
         self.body2cloud_trans.transform.rotation.x = quat[0]
         self.body2cloud_trans.transform.rotation.y = quat[1]
         self.body2cloud_trans.transform.rotation.z = quat[2]
@@ -86,7 +86,7 @@ class Repuber(Node):
         self.body2imu_trans.transform.translation.x = 0.0
         self.body2imu_trans.transform.translation.y = 0.0
         self.body2imu_trans.transform.translation.z = 0.0
-        quat = tf_transformations.quaternion_from_euler(3.14159265358, 0, 0)
+        quat = tf_transformations.quaternion_from_euler(0, 0, 0)
         self.body2imu_trans.transform.rotation.x = quat[0]
         self.body2imu_trans.transform.rotation.y = quat[1]
         self.body2imu_trans.transform.rotation.z = quat[2]
