777c31c3956645e367eff886819ea79492dbec1b
Liqun Xu <liqunxu@LiqundeMacBook-Pro.local>
2025-03-20 14:39:30 -0500

    change transform

--- src/utilities/transform_sensors/transform_sensors/transform_everything.py
+++ src/utilities/transform_sensors/transform_sensors/transform_everything.py
@@ -63,23 +63,23 @@ class Repuber(Node):
         self.ang_z2x_proj = calib_data['ang_z2x_proj']
         self.ang_z2y_proj = calib_data['ang_z2y_proj']
                 
-        self.body2cloud_trans = TransformStamped()
-        self.body2cloud_trans.header.stamp = self.get_clock().now().to_msg()
-        self.body2cloud_trans.header.frame_id = "body"
-        self.body2cloud_trans.child_frame_id = "unilidar_lidar"
-        self.body2cloud_trans.transform.translation.x = 0.0
-        self.body2cloud_trans.transform.translation.y = 0.0
-        self.body2cloud_trans.transform.translation.z = 0.0
-        quat = tf_transformations.quaternion_from_euler(0, 0, 0)
-        self.body2cloud_trans.transform.rotation.x = quat[0]
-        self.body2cloud_trans.transform.rotation.y = quat[1]
-        self.body2cloud_trans.transform.rotation.z = quat[2]
-        self.body2cloud_trans.transform.rotation.w = quat[3]
+        # self.body2cloud_trans = TransformStamped()
+        # self.body2cloud_trans.header.stamp = self.get_clock().now().to_msg()
+        # self.body2cloud_trans.header.frame_id = "body"
+        # self.body2cloud_trans.child_frame_id = "unilidar_lidar"
+        # self.body2cloud_trans.transform.translation.x = 0.0
+        # self.body2cloud_trans.transform.translation.y = 0.0
+        # self.body2cloud_trans.transform.translation.z = 0.0
+        # quat = tf_transformations.quaternion_from_euler(0, 0, 0)
+        # self.body2cloud_trans.transform.rotation.x = quat[0]
+        # self.body2cloud_trans.transform.rotation.y = quat[1]
+        # self.body2cloud_trans.transform.rotation.z = quat[2]
+        # self.body2cloud_trans.transform.rotation.w = quat[3]
         
         self.body2imu_trans = TransformStamped()
         self.body2imu_trans.header.stamp = self.get_clock().now().to_msg()
         self.body2imu_trans.header.frame_id = "body"
-        self.body2imu_trans.child_frame_id = "unilidar_imu"
+        self.body2imu_trans.child_frame_id = "unilidar_imu_initial"
         self.body2imu_trans.transform.translation.x = 0.0
         self.body2imu_trans.transform.translation.y = 0.0
         self.body2imu_trans.transform.translation.z = 0.0
