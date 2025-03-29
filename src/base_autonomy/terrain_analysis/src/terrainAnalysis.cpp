b16f802aec6710f654c5ab54d54464197b2537bf
Liqun Xu <liqunxu@LiqundeMacBook-Pro.local>
2025-03-29 00:06:48 -0500

    publish terrain map in camera_init first and then map

--- src/base_autonomy/terrain_analysis/src/terrainAnalysis.cpp
+++ src/base_autonomy/terrain_analysis/src/terrainAnalysis.cpp
@@ -29,7 +29,9 @@
 #include "rmw/types.h"
 #include "rmw/qos_profiles.h"
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+
+
 
 
 
